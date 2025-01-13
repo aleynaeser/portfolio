31fc8e45fc42df3216423018b103fc37d19eb415
Aleyna Eser <aleynaeser345.ma@gmail.com>
2025-01-13 23:08:23 +0300

    added header and about sections

---
+++ src/components/About.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <div className='pt-4 text-7xl font-semibold'>
+        <div>Get more out</div>
+
+        <div className='pt-6'>
+          <div>Creativity</div>
+        </div>
+      </div>
+
+      <div className='pt-9 text-xs leading-7 text-[var(--secondary)]'>
+        I have about 3 years of experience in software development, focusing on frontend. I actively contributed to all
+        stages of design, planning, and development in projects and expanded my mobile development skills using Flutter. I
+        specialize in creating user-friendly, creative interfaces and am constantly eager to learn new technologies. My aim
+        is to collaborate with a team where I can contribute my expertise and strong work ethic while gaining new
+        experiences.
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className='flex items-baseline justify-between'>
+      <div className='flex items-center justify-center gap-3'>
+        <div className='h-8 border-r border-[var(--primary)] pr-3 text-3xl font-semibold'>AE.</div>
+
+        <div className='flex flex-col gap-[2px]'>
+          <div className='font-medium'>Aleyna Eser</div>
+          <div className='text-xs font-extralight text-[var(--secondary)]'>Frontend Developer</div>
+        </div>
+      </div>
+
+      <div className='text-xs font-extralight text-[var(--secondary)]'>3 years of experience</div>
+    </header>
+  );
+}
