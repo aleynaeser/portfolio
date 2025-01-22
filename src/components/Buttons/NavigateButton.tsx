c6028cacb215d395083f997758e6727d66212fce
Aleyna Eser <aleynaeser345.ma@gmail.com>
2025-01-22 21:22:20 +0300

    updated button target

--- src/components/Buttons/NavigateButton.tsx
+++ src/components/Buttons/NavigateButton.tsx
@@ -11,6 +11,7 @@ export default function NavigateButton({ text, href, className, Icon }: INavigat
   return (
     <Link
       href={href}
+      target='_blank'
       className={`group relative flex w-fit items-center gap-2 overflow-hidden rounded-3xl border border-dark bg-neutral px-5 py-2 text-xs text-light ${className}`}
     >
       <span className='absolute inset-0 -translate-x-full bg-gradient-to-r from-transparent via-light to-transparent opacity-30 transition-transform duration-1000 group-hover:translate-x-full'></span>
